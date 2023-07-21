7110a35b36a229367b32bf70c2c77619d295d88e
Farukesgin <faruk00800jhii@gmail.com>
2023-07-21 12:40:19 +0300

    Karakter Animasyonu

--- Faruk Google Game Project/Assets/Scripts/OyuncuHareketleri.cs
+++ Faruk Google Game Project/Assets/Scripts/OyuncuHareketleri.cs
@@ -6,18 +6,47 @@ public class OyuncuHareketleri : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
 
     private void Awake()
     {
+
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        //Karakter yönü deðiþtirme
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1,1,1);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            jump();
+
+        //animatör deðerleri ayarlama
+        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
     }
+
+    private void jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag== "Ground")
+            grounded = true;
+    }
+
 }
